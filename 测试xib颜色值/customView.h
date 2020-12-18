9b23d5a4c450b41768925bf365b4d3d65c8e2b40
pengzhiqiang <zhiqiang.peng@ousutec.com.cn>
2020-12-18 22:51:46 +0800

    第一次

---
+++ 测试xib颜色值/customView.h
@@ -0,0 +1,17 @@
+//
+//  customView.h
+//  测试xib颜色值
+//
+//  Created by pd1 on 2020/12/18.
+//  Copyright © 2020 pd1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface customView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 测试xib颜色值/customView.m
@@ -0,0 +1,21 @@
+//
+//  customView.m
+//  测试xib颜色值
+//
+//  Created by pd1 on 2020/12/18.
+//  Copyright © 2020 pd1. All rights reserved.
+//
+
+#import "customView.h"
+
+@implementation customView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
